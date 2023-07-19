d517013aaf317dc238409e6d4eef8a5162d004d8
lifanjie <lifanjiemail@163.com>
2023-07-19 22:45:20 +0800

    remove carla-ros-bridge

--- src/carla_ros_bridge_ros/build/carla_ackermann_control/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/qrq/Documents/Shenlan_PnC/carla_ros_bridge_ros1/devel/.private/carla_ackermann_control/lib/python3/dist-packages:$PYTHONPATH
-exec /usr/bin/python3 "$@"
